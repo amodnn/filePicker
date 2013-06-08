04e4d12ff22d3946a6b613f18d8efdec83132626
Amod <amod.narvekar@rediffmail.com>
2013-06-08 22:50:55 +0530

    added pick functionality

---
+++ js/pickDemo.js
@@ -0,0 +1,48 @@
+function pickFile()
+{
+alert("pickFile called");
+var pick = new MozActivity({
+   name: "pick",
+   data: {
+	   type: ["text/html"]
+   } 
+});
+
+pick.onsuccess = function () {
+	var sec = document.getElementById("contentE");
+	 var f = new FileReader(); 
+	 f.readAsText(this.result.blob); 
+	 f.onload = function(evt) {
+	 alert("File received successfully.");
+	 sec.innerHTML=evt.target.result;
+	 }
+};
+ 
+pick.onerror = function () {
+    // If an error occurred or the user canceled the activity
+    alert("pick failed");
+};
+}
+
+function pickImageFile()
+{
+alert("function called");
+var pick = new MozActivity({
+   name: "pick",
+   data: {
+       type: ["image/png", "image/jpg", "image/jpeg"]
+   }
+});
+
+pick.onsuccess = function () {
+    var img = document.createElement("img");
+	var sec = document.getElementById("contentE");
+    img.src = window.URL.createObjectURL(this.result.blob);
+    sec.appendChild(img);
+};
+ 
+pick.onerror = function () {
+    // If an error occurred or the user canceled the activity
+    alert("Can't view the image!");
+};
+}
